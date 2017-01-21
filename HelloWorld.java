51fa4e3232e6c2fd371c7fec14a86f2ea3da5edd
Adarksoul <chadmanning222@gmail.com>
2017-01-20 18:02:38 -0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/* 
+* This program prints "Hello World". 
+* 
+* @author Chad Manning 
+* Course: COMP B11 
+* Created: Jan 18, 2017 
+* Source File: HelloWorld.java 
+*/ 
+
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("!!!Hello World!!!!"); //Outputs text
+	}
+)
